d28c76af03c54b1d43f414a71634d71bdc332aa6
Raphael Cussat <raphaelcussat93@gmail.com>
2023-07-06 00:15:11 -0300

    Exercício 04

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def reverse_sentence(sentence):
     words = sentence.split()
     reversed_sentence = " ".join(words[::-1])
     return reversed_sentence
-#Exercício 02
 
+#Exercício 02
 def remove_duplicates(string):
     unique_chars = ""
     for char in string:
@@ -18,6 +18,7 @@ def remove_duplicates(string):
             unique_chars += char
     return unique_chars.lower()
 
+#Exercicio 03
 def find_longest_palindroma(name):
     result = ''
     for i in range(len(name)):
@@ -29,10 +30,25 @@ def find_longest_palindroma(name):
                 if len(aux) > len(result):
                     result = aux
     return result
+
+#Exercicio 04
+def capitalize_sentences(name):
+    delimiters = ['? ', '. ', '! ', ': ', '; ']
+    def capitalize(string, delimiter):
+        sentences = string.split(delimiter)
+        capitalized_sentences = [sentence[0].upper() + sentence[1:] for sentence in sentences]
+        capitalized_string = delimiter.join(capitalized_sentences)
+        return capitalized_string
+
+    for delimiter in delimiters:
+        name = capitalize(name, delimiter)
+    return name
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(reverse_sentence('Ola PWC, eu me chamo Raphael'))
     print(remove_duplicates('Banana'))
     print(find_longest_palindroma('babdbabd'))
+    print(capitalize_sentences('olá. como você está? tudo bem? eu estou bem! obrigado.'))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
