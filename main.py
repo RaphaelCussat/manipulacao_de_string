db3e3ff20c0a7adedb47cf52054e12d141b679c1
Raphael Cussat <raphaelcussat93@gmail.com>
2023-07-06 01:21:14 -0300

    Testes unitarios

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import logging
 
 #Exercício 01
 def reverse_sentence(sentence):
